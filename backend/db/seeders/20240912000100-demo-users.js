01643448aec93bc0eb4086dc4f97f59a2bec4137
kevin <kchase07@gmail.com>
2024-09-16 14:11:16 -0500

    adjusted seeders

--- backend/db/seeders/20240912000100-demo-users.js
+++ backend/db/seeders/20240912000100-demo-users.js
@@ -10,14 +10,17 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await User.bulkCreate([
+    options.tableName = 'Users'
+    await User.bulkInsert([
       {
         id:1,
         firstName: 'Demo',
         lastName: 'Lition',
         email: 'demo@user.io',
         username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
+        hashedPassword: bcrypt.hashSync('password'),
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         id:2,
@@ -25,7 +28,9 @@ module.exports = {
         lastName: 'User',
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password2'),
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         id:3,
@@ -33,7 +38,9 @@ module.exports = {
         lastName: 'User',
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
+        hashedPassword: bcrypt.hashSync('password3'),
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ], { validate: true });
   },
@@ -41,9 +48,11 @@ module.exports = {
   async down (queryInterface, Sequelize) {
     options.tableName = 'Users';
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
+    return queryInterface.bulkDelete(options,
+      {
       username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
-    }, {});
+    },
+    {});
   }
 };
 
