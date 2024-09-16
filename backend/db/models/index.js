e71dcba92d2e5a2d64516db46520028fef63722e
kevin <kchase07@gmail.com>
2024-09-16 13:48:22 -0500

    index reset

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -1,7 +1,4 @@
 'use strict';
-const User = require('./user.js');
-const Spot = require('./spot.js');
-const Booking = require('./booking.js')
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
