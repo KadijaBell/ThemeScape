33ec831981cb0e8a6f26338ae303758b0e63af00
KadijaBell <kadija_bell@yahoo.com>
2024-10-24 20:39:52 -0400

    phase0 completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import configureStore from "./store/store";
-import { restoreCSRF, csrfFetch } from "./store/csrf";
+import configureStore from "./store/store.js";
+import { restoreCSRF, csrfFetch } from "./store/csrf.js";
 
 
 const store = configureStore();
@@ -24,3 +24,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       </Provider>
   </React.StrictMode>
 );
+
